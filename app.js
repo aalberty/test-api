3eb03ae53f77b448c53c1eee238c72bcd6e1ae06
aalberty <46965141+aalberty@users.noreply.github.com>
2023-02-04 17:43:21 -0500

    merging to one doc for ease of working example

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-var app = angular.module("myApp", [
-    '/controllers/apiCtrl.js',
-    '/controllers/mainCtrl.js'
-]);
+var app = angular.module("myApp", ['ngResource']);
 
-app.config(['$routeProvider'], function () {
-    $routeProvider
-    .when('/Test/:testParam', {
-        templateUrl: 'apiRes.html',
-        controller: 'apiCtrl'
-    });
+app.controller('mainCtrl', function ($scope) {
+    $scope.onClick() = onClick;
+    $scope.getReq() = getReq;
+    $scope.params = {
+        parm1: "",
+        parm2: ""
+    };
+    $scope.res = {
+        success: false,
+        data: {}
+    };
+
+    function onClick (message) {
+        console.log(message);
+    }
+
+    function getReq () {
+        let parm1 = $scope.parm1;
+        let parm2 = $scope.parm2;
+        $scope.res.success = true;
+        $scope.data = {parm1: parm1, parm2: parm2};
+    }
 });
