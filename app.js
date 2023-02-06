75d83789894e18fc81ed2e0c0e0119505c5c7c63
aalberty <46965141+aalberty@users.noreply.github.com>
2023-02-06 16:08:40 -0500

    refac

--- app.js
+++ app.js
@@ -12,14 +12,6 @@ app.controller('mainCtrl', function ($scope, $resource) {
         data: {}
     };
 
-    function getReq () {
-        let parm1 = $scope.params.parm1;
-        let parm2 = $scope.params.parm2;
-        $scope.res = {data: {}, success: false};
-        $scope.res.success = true;
-        $scope.res.data = {parm1: parm1, parm2: parm2};
-    }
-
     var restSvc = $resource("./getSample/:parm1/:parm2", 
     
         {
@@ -35,38 +27,47 @@ app.controller('mainCtrl', function ($scope, $resource) {
         }
     );
 
-    var prom = restSvc.getSample(
-        {
-            parm1: $scope.params.parm1,
-            parm2: $scope.params.parm2
-        }
-    ).$promise;
 
-    prom.then(
-        
-        function (res) {
-            let success = false;
-            let errorMessage = "";
-            let data = "";
+    function getReq () {
+        let parm1 = $scope.params.parm1;
+        let parm2 = $scope.params.parm2;
+        // $scope.res.success = true;
+        // $scope.res.data = {parm1: parm1, parm2: parm2};
 
-            if (res) {
-                success = true;
-                data = res;
-            } else {
-                success = false;
-                errorMessage = "No data returned.";
+        let prom = restSvc.getSample(
+            {
+                parm1: $scope.params.parm1,
+                parm2: $scope.params.parm2
             }
+        ).$promise;
+    
+        prom.then(
+            
+            function (res) {
+                let success = false;
+                let errorMessage = "";
+                let data = "";
+    
+                if (res) {
+                    success = true;
+                    data = res;
+                } else {
+                    success = false;
+                    errorMessage = "No data returned.";
+                }
+    
+                $scope.res.success = success;
+                $scope.res.errorMessage = errorMessage;
+                $scope.res.data = data;
+            },
+            function (error) {
+                $scope.res.success = false;
+                $scope.res.errorMessage = error;
+                $scope.res.data = {};
+            }
+        );
 
-            $scope.res.success = success;
-            $scope.res.errorMessage = errorMessage;
-            $scope.res.data = data;
-        },
-        function (error) {
-            $scope.res.success = false;
-            $scope.res.errorMessage = error;
-            $scope.res.data = {};
-        }
-    );
+    }
 });
 
 
