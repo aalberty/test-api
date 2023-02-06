65b1c2684e135f6c410400cc9fb03c3ec29bc5cc
aalberty <46965141+aalberty@users.noreply.github.com>
2023-02-06 12:48:58 -0500

    refactor to single js file

--- controllers/mainCtrl.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module("mainCtrl", []);
-app.controller("mainCtrl", function ($scope){
-    $scope.onClick = onClick;
-
-    function onClick () {
-        console.log("button was clicked");
-    }
-});
