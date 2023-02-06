ed93caa9c77aca5af8b9e993d62743dc18467bb9
aalberty <46965141+aalberty@users.noreply.github.com>
2023-02-06 16:05:45 -0500

    attempting resource

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module("myApp", ['ngResource']);
 
-app.controller('mainCtrl', function ($scope) {
+app.controller('mainCtrl', function ($scope, $resource) {
     $scope.getReq = getReq;
     $scope.params = {
         parm1: "",
@@ -8,6 +8,7 @@ app.controller('mainCtrl', function ($scope) {
     };
     $scope.res = {
         success: false,
+        errorMessage: "",
         data: {}
     };
 
@@ -18,4 +19,54 @@ app.controller('mainCtrl', function ($scope) {
         $scope.res.success = true;
         $scope.res.data = {parm1: parm1, parm2: parm2};
     }
+
+    var restSvc = $resource("./getSample/:parm1/:parm2", 
+    
+        {
+            parm1: "@parm1",
+            parm2: "@parm2"
+        },
+
+        {
+            "getSample": {
+                method: "get",
+                isArray: "false"
+            }    
+        }
+    );
+
+    var prom = restSvc.getSample(
+        {
+            parm1: $scope.params.parm1,
+            parm2: $scope.params.parm2
+        }
+    ).$promise;
+
+    prom.then(
+        
+        function (res) {
+            let success = false;
+            let errorMessage = "";
+            let data = "";
+
+            if (res) {
+                success = true;
+                data = res;
+            } else {
+                success = false;
+                errorMessage = "No data returned.";
+            }
+
+            $scope.res.success = success;
+            $scope.res.errorMessage = errorMessage;
+            $scope.res.data = data;
+        },
+        function (error) {
+            $scope.res.success = false;
+            $scope.res.errorMessage = error;
+            $scope.res.data = {};
+        }
+    );
 });
+
+
